3f2d05247ad621df6d3bef80fac5c95dc54c0696
MysticalDream <1817764079@qq.com>
2023-02-10 21:18:37 +0800

    feat: 添加阻塞队列

--- glutemo/src/main/java/com/mysticaldream/glutemo/concurrent/SimpleTaskLoopExecutor.java
+++ glutemo/src/main/java/com/mysticaldream/glutemo/concurrent/SimpleTaskLoopExecutor.java
@@ -1,25 +1,38 @@
 package com.mysticaldream.glutemo.concurrent;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * @author MysticalDream
  */
+@Slf4j
 public class SimpleTaskLoopExecutor extends AbstractTaskLoopExecutor {
 
 
     public SimpleTaskLoopExecutor(Executor executor) {
-        super(executor);
+        super(new LinkedBlockingQueue<>(), executor);
     }
 
     @Override
     protected void run() {
         while (true) {
-            processTasks();
-            if (isShutdown()) {
-                break;
+            try {
+                processTasks();
+                if (isShutdown()) {
+                    break;
+                }
+            } catch (Exception e) {
+                log.error("exception", e);
             }
         }
+    }
+
+    @Override
+    protected void processTasks() throws InterruptedException {
+        ((LinkedBlockingQueue<Runnable>) tasks()).take().run();
     }
 
     @Override
